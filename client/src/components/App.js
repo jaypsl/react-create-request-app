ab7a389a61f1996ca0f1ef49a68e4508a9fc97ad
Jaypal Solanki <jaypsl263@gmail.com>
2018-11-13 00:42:57 -0500

    request app

---
+++ client/src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import Nav from './Nav';
+import PageNotFound from './PageNotFound';
+import ProtectedRoute from './ProtectedRoute';
+// import OpeningCrawContainer from './starwar/OpeningCrawContainer'
+
+import Register from './user/Register';
+import Login from './user/Login';
+// import HighchartsComponent from './Highcharts';
+
+import CreateRequest from './request/Create';
+import ListRequest from './request/List';
+// import UploadFile from './upload/Upload';
+import Test from './Test';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Switch>
+          <ProtectedRoute path='/' component={Home} exact={true} />
+          <ProtectedRoute path='/createRequest' component={CreateRequest} />
+          <ProtectedRoute path='/listRequest' component={ListRequest} />
+          {/* <ProtectedRoute path='/highcharts' component={HighchartsComponent} />
+                    <ProtectedRoute path='/starwar' component={OpeningCrawContainer} /> */}
+          {/* <Route path='/uploadFile' component={UploadFile} /> */}
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/test/' component={Test} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/user/Login.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import serializeForm from 'form-serialize';
+import { InputGroup, Input } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
+
+import { login } from '../../redux/actions/user';
+import LoadingButton from '../util/LoadingButton';
+
+class Login extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        var user = serializeForm(e.target, { hash: true });
+        this.props.dispatch(login(user)).then(d => {
+            this.props.history.push('/');
+        });
+    }
+
+    render() {
+        return (
+            <div className='register-info-box'>
+                <form onSubmit={this.handleSubmit}>
+                    <InputGroup>
+                        <Input placeholder="username" name="username" type='text'/>
+                    </InputGroup>
+                    <br />
+                    <InputGroup>
+                        <Input placeholder="password" name="password" type='password'/>
+                    </InputGroup>
+                    <br />
+                    <LoadingButton type="submit" finish_to='/' tag="Login" value="Login" />
+                    {/* <Button color="primary" type="submit" disabled={this.props.loading === LOADING_PENDING}>
+                        {this.props.loading !== LOADING_PENDING ? "Login!" : "Loading"}
+                        {this.props.loading === LOADING_FINISHED && <Redirect to='/' />}
+                    </Button> */}
+                </form>
+            </div>
+        );
+    }
+}
+
+export default withRouter(connect()(Login));
